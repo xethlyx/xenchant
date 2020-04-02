ba29eba30a1a4a00338344009e2ce7666dbe5c97
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 19:29:57 -0400

    Fixed no argument exception

--- src/main/java/com/xethlyx/plugins/xenchant/commands/XEnchantCommand.java
+++ src/main/java/com/xethlyx/plugins/xenchant/commands/XEnchantCommand.java
@@ -1,5 +1,6 @@
 package com.xethlyx.plugins.xenchant.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -7,6 +8,10 @@ import org.bukkit.command.CommandSender;
 public class XEnchantCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (args.length < 1) {
+            return false;
+        }
+
         switch(args[0]) {
             case "update": {
                 sender.sendMessage("Checking for updates..");
