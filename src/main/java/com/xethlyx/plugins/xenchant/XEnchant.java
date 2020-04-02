f16406e66afaf54e2476331cb486cc7b38f63142
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 17:30:00 -0400

    Add default implementation for XEnchant

--- src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
+++ src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
@@ -1,11 +1,12 @@
 package com.xethlyx.plugins.xenchant;
 
+import com.xethlyx.plugins.xenchant.commands.XEnchantCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XEnchant extends JavaPlugin {
     @Override
     public void onEnable() {
-
+        this.getCommand("xenchant").setExecutor(new XEnchantCommand());
     }
 
     @Override
---
+++ src/main/java/com/xethlyx/plugins/xenchant/commands/XEnchantCommand.java
@@ -0,0 +1,24 @@
+package com.xethlyx.plugins.xenchant.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class XEnchantCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        switch(args[0]) {
+            case "update": {
+                sender.sendMessage("Checking for updates..");
+            }
+            case "reload": {
+                sender.sendMessage("Reloading..");
+            }
+            default: {
+                sender.sendMessage("Applying enchant " + args[0]);
+            }
+        }
+    }
+}
