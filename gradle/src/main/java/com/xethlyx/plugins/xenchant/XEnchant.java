d9547c3eb08c28a757927f3731a52e2371dffa73
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 00:57:42 -0400

    Gradle update and gitignore

---
+++ gradle/src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
@@ -0,0 +1,15 @@
+package com.xethlyx.plugins.xenchant;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class XEnchant extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
