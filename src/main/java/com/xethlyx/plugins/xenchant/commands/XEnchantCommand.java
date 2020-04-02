0e03e94ee8eba98d087de6e86c37e77202f70a69
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 18:00:05 -0400

    Fix switch statement inside cmd

--- src/main/java/com/xethlyx/plugins/xenchant/commands/XEnchantCommand.java
+++ src/main/java/com/xethlyx/plugins/xenchant/commands/XEnchantCommand.java
@@ -3,18 +3,18 @@ package com.xethlyx.plugins.xenchant.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class XEnchantCommand implements CommandExecutor {
-
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         switch(args[0]) {
             case "update": {
                 sender.sendMessage("Checking for updates..");
+                break;
             }
             case "reload": {
                 sender.sendMessage("Reloading..");
+                break;
             }
             default: {
                 sender.sendMessage("Applying enchant " + args[0] + "..");
