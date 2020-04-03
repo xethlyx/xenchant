334440d8de41e2448f6b580057f913baee52c65d
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 23:14:27 -0400

    Update XEnchant.java

--- src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
+++ src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
@@ -1,7 +1,6 @@
 package com.xethlyx.plugins.xenchant;
 
 import com.xethlyx.plugins.xenchant.commands.XEnchantCommand;
-import com.xethlyx.plugins.xenchant.enchants.Beheading;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -11,7 +10,7 @@ public class XEnchant extends JavaPlugin {
         getLogger().info("Starting initialization of enchants..");
         this.getCommand("xenchant").setExecutor(new XEnchantCommand());
 
-        new Beheading().registerEnchant();
+        // new Beheading().registerEnchant();
 
         Bukkit.broadcastMessage("There are currently " + EnchantRegistry.EnchantList.size() + " enchants registered.");
         getLogger().info("There are currently " + EnchantRegistry.EnchantList.size() + " enchants registered.");
