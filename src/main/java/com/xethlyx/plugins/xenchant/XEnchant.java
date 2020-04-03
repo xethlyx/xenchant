ac8e6d003dc4ca63247b35440b080637cd335c7d
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-02 22:30:49 -0400

    Logger is back down to info, broadcasts too

--- src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
+++ src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
@@ -2,19 +2,22 @@ package com.xethlyx.plugins.xenchant;
 
 import com.xethlyx.plugins.xenchant.commands.XEnchantCommand;
 import com.xethlyx.plugins.xenchant.enchants.Beheading;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class XEnchant extends JavaPlugin {
     @Override
     public void onEnable() {
+        getLogger().info("Starting initialization of enchants..");
         this.getCommand("xenchant").setExecutor(new XEnchantCommand());
 
         EnchantRegistry.registerEnchant(new Beheading());
 
-        getLogger().warning("There are currently " + EnchantRegistry.EnchantList.size() + " enchants registered.");
+        Bukkit.broadcastMessage("There are currently " + EnchantRegistry.EnchantList.size() + " enchants registered.");
+        getLogger().info("There are currently " + EnchantRegistry.EnchantList.size() + " enchants registered.");
 
         for (Enchant enchant : EnchantRegistry.EnchantList) {
-            getLogger().warning("Registering listener for enchantment " + enchant.Name + "...");
+            getLogger().info("Registering listener for enchantment " + enchant.Name + "...");
             getServer().getPluginManager().registerEvents(enchant.Listener, this);
         }
     }
