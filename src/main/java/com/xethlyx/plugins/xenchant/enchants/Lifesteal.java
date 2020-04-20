fa87c60210afcc0635f7ab038b993fdedffafdb8
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2020-04-19 23:14:00 -0500

    Create Lifesteal.java

---
+++ src/main/java/com/xethlyx/plugins/xenchant/enchants/Lifesteal.java
@@ -0,0 +1,41 @@
+package com.xethlyx.plugins.xenchant.enchants;
+
+import com.xethlyx.plugins.xenchant.Enchant;
+import com.xethlyx.plugins.xenchant.EnchantRegistry;
+import com.xethlyx.plugins.xenchant.util.EnchantRegister;
+import org.bukkit.Material;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lifesteal extends EnchantRegister {
+    @Override
+    public void registerEnchant() {
+        Enchant lifestealEnchant = new Enchant<lifestealListeners>();
+
+        lifestealEnchant.Name = "Lifesteal";
+        lifestealEnchant.Description = "A random percentage of the damage you deal to enemies is used to heal you.";
+
+        lifestealEnchant.MaxLevel = 4;
+
+        List<Material> allowedItems = new ArrayList<>();
+
+        allowedItems.add(Material.DIAMOND_SWORD);
+        allowedItems.add(Material.IRON_SWORD);
+        allowedItems.add(Material.GOLDEN_SWORD);
+        allowedItems.add(Material.STONE_SWORD);
+        allowedItems.add(Material.WOODEN_SWORD);
+
+        allowedItems.add(Material.DIAMOND_AXE);
+        allowedItems.add(Material.IRON_AXE);
+        allowedItems.add(Material.GOLDEN_AXE);
+        allowedItems.add(Material.STONE_AXE);
+        allowedItems.add(Material.WOODEN_AXE);
+
+        lifestealEnchant.AllowedItems = allowedItems;
+
+        lifestealEnchant.EnchantListener = new lifestealListeners();
+
+        EnchantRegistry.registerEnchant("lifesteal", lifestealEnchant);
+    }
+}
