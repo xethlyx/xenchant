ff644d4032efd80229ead226e358ff5077d277b8
TestingPlant <44930139+TestingPlant@users.noreply.github.com>
2020-04-20 01:05:58 -0500

    Update Lifesteal.java
    Capitalized correctly

--- src/main/java/com/xethlyx/plugins/xenchant/enchants/Lifesteal.java
+++ src/main/java/com/xethlyx/plugins/xenchant/enchants/Lifesteal.java
@@ -11,7 +11,7 @@ import java.util.List;
 public class Lifesteal extends EnchantRegister {
     @Override
     public void registerEnchant() {
-        Enchant lifestealEnchant = new Enchant<lifestealListeners>();
+        Enchant lifestealEnchant = new Enchant<LifestealListeners>();
 
         lifestealEnchant.Name = "Lifesteal";
         lifestealEnchant.Description = "A random percentage of the damage you deal to enemies is used to heal you.";
@@ -34,7 +34,7 @@ public class Lifesteal extends EnchantRegister {
 
         lifestealEnchant.AllowedItems = allowedItems;
 
-        lifestealEnchant.EnchantListener = new lifestealListeners();
+        lifestealEnchant.EnchantListener = new LifestealListeners();
 
         EnchantRegistry.registerEnchant("lifesteal", lifestealEnchant);
     }
