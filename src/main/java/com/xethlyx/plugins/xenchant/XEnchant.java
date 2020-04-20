5887555cff084272cb234255e49969415d4aa514
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-04-20 11:25:36 -0400

    Register lifesteal

--- src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
+++ src/main/java/com/xethlyx/plugins/xenchant/XEnchant.java
@@ -26,6 +26,7 @@ public class XEnchant extends JavaPlugin {
         new Beheading().registerEnchant();
         new Smelting().registerEnchant();
         new Propulsion().registerEnchant();
+        new Lifesteal().registerEnchant();
 
         // Do other stuff
 
