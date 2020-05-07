78d23e52e8ad71876c0a0a10835c7dc24b77742d
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-05-07 19:31:49 -0400

    Added debug msgs

--- src/main/java/com/xethlyx/plugins/xenchant/enchants/InsomniacListeners.java
+++ src/main/java/com/xethlyx/plugins/xenchant/enchants/InsomniacListeners.java
@@ -1,5 +1,6 @@
 package com.xethlyx.plugins.xenchant.enchants;
 
+import com.xethlyx.plugins.xenchant.XEnchant;
 import com.xethlyx.plugins.xenchant.util.EnchantUtil;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Phantom;
@@ -11,23 +12,35 @@ import java.util.Random;
 
 public class InsomniacListeners implements Listener {
     public void onEntityTarget(EntityTargetLivingEntityEvent event) {
-        if (!(event.getTarget() instanceof Player)) return;
-        if (!(event.getEntity() instanceof Phantom)) return;
+        if (!(event.getTarget() instanceof Player)) {
+            XEnchant.Instance.getLogger().info("Not player");
+            return;
+        }
+        if (!(event.getEntity() instanceof Phantom)) {
+            XEnchant.Instance.getLogger().info("Not phantom");
+            return;
+        }
 
         // Phantom target player
         Player player = (Player) event.getEntity();
         LivingEntity entity =  (LivingEntity) event.getEntity();
 
+        XEnchant.Instance.getLogger().info("Passed checks");
+
         int enchantLevel = EnchantUtil.parseEnchant("insomniac", player.getInventory().getHelmet());
 
+
         if (enchantLevel == 0) return;
+        XEnchant.Instance.getLogger().info("Passed insomniac check");
 
         float repelChance = Math.min(enchantLevel * 0.4f, 1);
 
         if ((new Random().nextFloat()) > (repelChance)) return;
+        XEnchant.Instance.getLogger().info("Passed rngesus");
 
         // RNGesus has approved
 
+        XEnchant.Instance.getLogger().info("Killing phantom");
         entity.setHealth(0);
     }
 }
