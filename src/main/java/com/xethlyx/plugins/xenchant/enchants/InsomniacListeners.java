5f406f77c787d0d4c2384c18b92c08ac7403285a
xethlyx <46338199+xethlyx@users.noreply.github.com>
2020-05-07 19:33:08 -0400

    Added eventhandler decorator

--- src/main/java/com/xethlyx/plugins/xenchant/enchants/InsomniacListeners.java
+++ src/main/java/com/xethlyx/plugins/xenchant/enchants/InsomniacListeners.java
@@ -5,12 +5,14 @@ import com.xethlyx.plugins.xenchant.util.EnchantUtil;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Phantom;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityTargetLivingEntityEvent;
 
 import java.util.Random;
 
 public class InsomniacListeners implements Listener {
+    @EventHandler
     public void onEntityTarget(EntityTargetLivingEntityEvent event) {
         if (!(event.getTarget() instanceof Player)) {
             XEnchant.Instance.getLogger().info("Not player");
